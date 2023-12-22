d25b86492f9b5edb91324aa0b20942498199eee9
TheXnator <trexnatorgaming@gmail.com>
2023-12-22 16:55:00 +0000

    Day 3

--- Day3.cs
+++ Day3.cs
@@ -1,9 +1,12 @@
+using System.Collections;
+using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
 
 class Day3() {
     public static void GetSolutions() {
         Console.WriteLine(string.Format("Day 3 part 1: {0}", EnginePartSum()));
-        //Console.WriteLine(string.Format("Day 3 part 2: {0}", EnginePartSum()));
+        Console.WriteLine(string.Format("Day 3 part 2: {0}", GearRatios()));
     }
 
     static bool IsSchematicSymbol(char c) {
@@ -41,6 +44,71 @@ class Day3() {
                         sum += Convert.ToInt32(regMatch.Value);
                         break;
                     }
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    static int[] GetGearNums(string checkString, int matchLocation, string originalLine) {
+        int startSearch = matchLocation > 2 ? matchLocation - 3 : 0;
+        int searchLength = matchLocation < (checkString.Length - 3) ? 7 : 4;
+        string revisedCheckString = checkString.Substring(startSearch, searchLength);
+
+        int searchLengthOriginal = matchLocation < (originalLine.Length - 4) ? 7 : 4;
+        string revisedOriginalLineString = originalLine.Substring(startSearch, searchLengthOriginal);
+        int originalMatchLocation = Regex.Matches(revisedOriginalLineString, @"\*").First().Index;
+
+        List<int> nums = new List<int>();
+
+        MatchCollection regMatchCollection = Regex.Matches(revisedCheckString, @"[\d]+");
+        foreach (Match regMatch in regMatchCollection) {
+            var index = regMatch.Index;
+            var matchEnd = index + regMatch.Value.Length;
+            
+            if ((matchEnd >= originalMatchLocation + 1 && index < originalMatchLocation + 1) || (index <= originalMatchLocation + 1 && matchEnd >= originalMatchLocation)) {
+                nums.Add(Convert.ToInt32(regMatch.Value));
+            }
+        }
+
+        return nums.ToArray();
+    }
+
+    static int GearRatios() {
+        int sum = 0;
+
+        string filename = "inputs/day3inputs.txt";
+        string[] contents = File.ReadAllLines(filename);
+
+        for (int x = 0; x < contents.Length; x++)
+        {
+            MatchCollection regMatchCollection = Regex.Matches(contents[x], @"\*");
+            foreach (Match regMatch in regMatchCollection) {
+                var index = regMatch.Index;
+                var matchEnd = index + regMatch.Value.Length;
+
+                int[] prevLineNums = GetGearNums(contents[x - 1], index, contents[x]);
+                int[] thisLineNums = GetGearNums(contents[x], index, contents[x]);
+                int[] nextLineNums = GetGearNums(contents[x + 1], index, contents[x]);
+
+                int totalGears = prevLineNums.Length + thisLineNums.Length + nextLineNums.Length;
+                if (totalGears == 2) {
+                    int val = 1;
+
+                    foreach(int num in prevLineNums) {
+                        val *= num;
+                    }
+
+                    foreach(int num in thisLineNums) {
+                        val *= num;
+                    }
+
+                    foreach(int num in nextLineNums) {
+                        val *= num;
+                    }
+
+                    sum += val;
                 }
             }
         }
--- obj/Debug/net8.0/AdventOfCode2023.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode2023.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2023")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98b46b2dd34cb50efe4abcd5a57d147e63400991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+059f12552877be5aeef9e7532012197b533b2d28")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2023")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2023")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
